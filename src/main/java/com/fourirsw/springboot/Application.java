dbfc99f75487afdb493168b9b589a43c36d83512
HaSense <virtual_sense@naver.com>
2021-07-16 16:14:06 +0900

    upload

--- src/main/java/com/fourirsw/springboot/Application.java
+++ src/main/java/com/fourirsw/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing  //BaseTiimeEntity을 이용하여 시간을 입력하려면 삽입하여야 한다.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
